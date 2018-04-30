3742d9def7a376c89dcc38354b6ef4ff4e34b23d
Lobsang Maldonado <lobsang41@gmail.com>
2018-04-29 21:21:21 -0500

    base

---
+++ js/interaction.js
@@ -0,0 +1,25 @@
+AFRAME.registerComponent('scale-on-mouseenter', {
+   schema: {
+     to: {default: '2.5 2.5 2.5'}
+   },
+
+   init: function () {
+     var data = this.data;
+     this.el.addEventListener('mouseenter', function () {
+       this.setAttribute('scale', data.to);
+     });
+   }
+ });
+
+ AFRAME.registerComponent('scale-on-mouseleave', {
+    schema: {
+      to: {default: '2.5 2.5 2.5'}
+    },
+
+    init: function () {
+      var data = this.data;
+      this.el.addEventListener('mouseleave', function () {
+        this.setAttribute('scale', data.to);
+      });
+    }
+  });
